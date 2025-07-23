0d8ee434794a6590a0867d01d9d5c6d38ea18f7d
rishithareddy6-hub <2420030541@klh.edu.in>
2025-07-23 11:33:44 +0530

    Add files via upload

---
+++ autoboxing.java
@@ -0,0 +1,10 @@
+package oops1;
+
+
+	public class autoboxing {
+	    public static void main(String[] args) {
+	        int num = 10;
+	        Integer obj = num; // autoboxing
+	        System.out.println(obj);
+	    }
+	}

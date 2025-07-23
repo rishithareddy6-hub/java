620d0a7fc98f48a90fda93e89c7215211e518593
rishithareddy6-hub <2420030541@klh.edu.in>
2025-07-23 11:29:31 +0530

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,10 @@
+package oops1;
+
+public class TypeCasting {
+	    public static void main(String[] args) {
+	        double d = 10.75;
+	        int n = (int) d; // double to int (explicit casting)
+	        System.out.println("Double value: " + d);
+	        System.out.println("Cast to int: " + n);
+	    }
+	}
---
+++ TypeConversation.java
@@ -0,0 +1,10 @@
+package oops1;
+
+public class TypeConversation {
+    public static void main(String[] args) {
+        int n = 100;
+        double d = n; // int to double (implicit conversion)
+        System.out.println("Integer value: " + n);
+        System.out.println("Converted to double: " + d);
+    }
+}
